6229425637b9cf402a4347a9de3d5d300c5ad8af
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-12-13 19:58:33 +0100

    Added ContactSection

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,18 +6,23 @@
         <section>
             <ProcessSection />
         </section>
+        <section>
+            <ContactSection />
+        </section>
     </div>
 </template>
 
 <script>
 import IllustrateSection from "./main-components/IllustrateSection.vue";
 import ProcessSection from "./main-components/ProcessSection.vue";
+import ContactSection from "./main-components/ContactSection.vue";
 
 export default {
     name: "Main",
     components: {
         IllustrateSection,
         ProcessSection,
+        ContactSection,
     },
 };
 </script>
---
+++ src/components/main-components/ContactSection.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="contact-section">
+        <div class="container">
+            <h2 class="ls-title">
+                Want to work with us? Send us
+                <span class="ls-title__italic">a message!</span>
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commo.
+            </p>
+            <div class="ls-button text-center">
+                <span class="ls-button-text">Contact</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Contact",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/vars.scss";
+
+.contact-section {
+    background-image: url(../../assets/img/h-2-background-1.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+    padding: 155px 0;
+    p {
+        font-size: 20px;
+        color: $color-secondary;
+        text-align: center;
+    }
+}
+</style>
